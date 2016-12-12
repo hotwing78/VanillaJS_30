27f7603ef525ca5edc6bc832628f268ffe407664
hotwing78 <dlwingo@gmail.com>
2016-12-12 00:23:33 -0500

    clock is a little glitchy still

---
+++ Clock/clockWork.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  if(secondsDegrees === 444){
+    secondHand.style.transition = null;
+  }
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((mins / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
