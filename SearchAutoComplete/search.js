9a566f1cbd5d577bc22d394503af8255b1a6b535
hotwing78 <dlwingo@gmail.com>
2016-12-15 23:56:00 -0500

    THIS was a cool one

---
+++ SearchAutoComplete/search.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json',
+    cities = [],
+    search = document.querySelector('.search'),
+    suggest = document.querySelector('.suggestions');
+
+    fetch(endpoint)
+        .then(blob=> blob.json())
+        .then(data => cities.push(...data));//I LOVE SPREAD!!!!!!
+        console.log(cities);
+
+
+function wordMatch(word, cities){
+    return cities.filter(place=>{
+      const regex = new RegExp(word,'gi');
+      return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function matchDisplay(){
+  const matchesArray = wordMatch(this.value, cities);
+  const html = matchesArray.map( place => {
+    const regex = new RegExp(this.value,'gi');
+    const cityName = place.city.replace(regex,`<span class = 'hl'>${this.value}</span>`);
+    const stateName = place.state.replace(regex,`<span class = 'hl'>${this.value}</span>`);
+    return `
+            <li>
+              <span class = 'name'>${cityName}, ${stateName}</span>
+              <span class = 'population'> ${numberWithCommas(place.population)}</span>
+            <li>`
+  }).join('');
+  suggest.innerHTML = html;
+}
+
+search.addEventListener('change', matchDisplay);
+search.addEventListener('keyup', matchDisplay);
