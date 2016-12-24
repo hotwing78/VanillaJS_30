8d06c056062b45b15fac883fb970a31760afcd32
hotwing78 <dlwingo@gmail.com>
2016-12-23 21:38:10 -0500

    Can't find method to turn cam off

--- MouseShadow/app.js
+++ MouseShadow/app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+document.write('This is forgotten');
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 const walk = 100; //100px
@@ -11,7 +11,7 @@ function shadow(e){
   let{offsetX: x, offsetY: y} = e;
 
   if(this !== e.target){
-    x = x + e.target.offsetLeft;
+  x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
 
